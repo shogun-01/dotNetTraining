e3b093f8b2a4754923feae9b26218fcf7105a9dd
unknown <AraSub2k1@gmail.com>
2022-07-12 17:31:38 +0530

    Added Custom Exception Files

---
+++ CustomExceptionDemo/CustomExceptionDemo/CSVNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace CustomExceptionDemo
+{
+    public class CSVNotFoundException : Exception
+    {
+
+        public CSVNotFoundException()
+        {
+        }
+        public CSVNotFoundException(string message): base(message)
+        {
+        }
+        public CSVNotFoundException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ CustomExceptionDemo/CustomExceptionDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CustomExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a absolute path for a CSV file: ");
+                var FilePath = Console.ReadLine();
+                if (FilePath.Substring(FilePath.Length - 3) != "csv")
+                {
+                    throw new CSVNotFoundException("Not a CSV file");
+                }
+                File.OpenRead(FilePath);
+                Console.WriteLine("File opened successfully");
+                Console.WriteLine("Reading csv file");
+
+                // File.OpenRead("E:\\Ara\\data\\auth.txt");
+                // the above line will throw an exception
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("File closed successfully");
+            }
+            Console.ReadLine();
+        }
+    }
+}
